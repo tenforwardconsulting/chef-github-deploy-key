91958127febe1b63f77b630481f360a150e73057
Graham Christensen <gchristensen@nationalfield.org>
2013-05-07 18:20:43 -0400

    License.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             "github-deploy-key"
 maintainer       "ZippyKid Inc"
 maintainer_email "support@zippykid.com"
-license          "All rights reserved"
+license          "GPL v3"
 description      "Generates and registers GitHub Deployment Keys"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
